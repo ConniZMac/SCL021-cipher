737ddfff2e0af8b1e0698cc1d6719b493c60d694
ConniZMac <constanza.macnamara@gmail.com>
2022-07-21 23:30:25 -0400

    Terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,6 @@
 const cipher = {
     encode: function(desplazamiento,texto) {
-      
+       
    
      if (!texto) 
          return ''; 
@@ -8,14 +8,17 @@ const cipher = {
      
      desplazamiento = (desplazamiento % 26 + 26) % 26; 
      return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) + desplazamiento) % 26]);
+     
    },
    
    decode: function(desplazamiento, texto) {
+   
      if (!texto) 
          return ''; 
      const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
      desplazamiento = (desplazamiento % 26 - 26) % 26; 
      return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - desplazamiento) % 26]);
+     
    }
    
    };
